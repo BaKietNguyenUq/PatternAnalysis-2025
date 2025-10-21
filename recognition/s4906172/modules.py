2d99bb358b90c65ff42c79d8bc56523421a8c086
Ba Kiet Nguyen Uq <bakiet.nguyen@student.uq.edu.au>
2025-10-21 11:34:20 +1000

    Implement TripletLoss to calculate the loss of anchor/positive/negative

--- recognition/s4906172/modules.py
+++ recognition/s4906172/modules.py
@@ -80,8 +80,25 @@ class SiameseNetwork(nn.Module):
         embedding = self.get_embedding(x)
         return self.classifier(embedding)
     
+class TripletLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def calc_euclidean(self, x1, x2):
+        return (x1 - x2).pow(2).sum(1)
+
+    def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor) -> torch.Tensor:
+        distance_positive = self.calc_euclidean(anchor, positive)
+        distance_negative = self.calc_euclidean(anchor, negative)
+        losses = torch.relu(distance_positive - distance_negative + self.margin)
+
+        return losses.mean()
     
 def get_config() -> dict:
+    """
+    Return config for data paths and training hyperparameters
+    """
     config = {
         "metadata_path": "./data/train-metadata.csv",
         "image_path": "./data/train-image/image/",
